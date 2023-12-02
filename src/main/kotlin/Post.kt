271a6eef1701f6efa31367119e488c6a5271c508
Dmitry <boetz@yandex.ru>
2023-12-02 23:51:51 +0700

    Add commit

---
+++ src/main/kotlin/Post.kt
@@ -0,0 +1,63 @@
+data class Post(
+    var id: Int = 0,
+    val ownerId: Int = 0,
+    val createdBy: Int = 0,
+    val date: Int = 0,
+    var text: String,
+    val canPin: Boolean = true,
+    val canDelete: Boolean = true,
+    val isPinned: Boolean = false,
+    val markedAsAds: Boolean = false,
+    val original: Post? = null,
+    var likes: Like = Like(),
+    var comment: Comment? = null
+) {
+    data class Like(
+        val count: Int = 0,
+        val user_likes: Boolean = false,
+        val can_like: Boolean = true,
+        val can_publish: Boolean = false
+    ) {
+    }
+}
+
+class PostNotFoundException(message: String) : RuntimeException(message)
+
+object WallService {
+    var posts = emptyArray<Post>()
+    var postId: Int = 0
+    var comments = emptyArray<Comment>()
+
+    fun createComment(postId: Int, comment: Comment)  {
+            for ((index, post) in posts.withIndex()) {
+                if (post.id == postId) {
+                    comments += comment.copy()
+                    //return comment
+                }
+                throw PostNotFoundException ("no post with such ID $postId")
+            }
+    }
+
+    fun clear() {
+        posts = emptyArray()
+        postId = 0
+    }
+
+    fun add(post: Post): Post {
+        posts += post.copy(id = ++postId, likes = post.likes.copy())
+        return posts.last()
+    }
+
+    fun update(newPost: Post): Boolean {
+        for ((index, post) in posts.withIndex()) {
+            if (post.id == newPost.id) {
+                posts[index] = newPost.copy()
+                println(posts[index])
+                return true
+            }
+        }
+        return false
+    }
+}
+
+
